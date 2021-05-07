a6b720817d43a8743809dffe8a274d5c9422527a
github username <github email address>
2021-05-07 00:46:59 -0400

    practice writing getters and setters in classes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your Circle class here
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius * 2;
+    }
+    get circumference(){
+        return 2 * Math.PI * this.radius;
+    }
+    get area(){
+        return Math.PI * Math.pow(this.radius, 2)
+    }
+
+    set diameter(value){
+        return this.radius = value / 2;
+    }   
+    set circumference(value){
+        return this.radius = value / Math.PI / 2;
+    }
+    set area(value){
+        return this.radius = Math.sqrt(value) / Math.PI
+    }
+}
